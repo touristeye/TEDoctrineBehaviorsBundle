5dec6aba5228e8c7335982ef030919c652e54642
Javier F. Escribano <javier@touristeye.com>
2013-04-14 13:42:44 +0200

    The models can have private fields, bindables if enabled
    The models can have three type of fields:
    - Always bindable
    - Bindable if $canChangePrivateFields=true
    - Not bindable
    
    With this change we can control better the private fields that only the developers can change when do the user does something.

--- Model/JSONBindable.php
+++ Model/JSONBindable.php
@@ -7,13 +7,18 @@ trait JSONBindable {
     /**
      * Bind params into the entity
      *
-     * @param array $params
+     * @param  array    $params
+     * @param  boolean  $canChangePrivateFields
      */
-    public function bind($params) {
+    public function bind($params, $canChangePrivateFields=false) {
+
+        // get the fields that we can change
+        $bindableParams = $canChangePrivateFields && isset(self::$privateParams)
+            ? array_merge(self::$allowedParams, self::$privateParams)
+            : self::$allowedParams;
 
         // get the allowed params
-        $cleanedParams = array_intersect_key($params,
-            array_flip(self::$allowedParams));
+        $cleanedParams = array_intersect_key($params, array_flip($bindableParams));
 
         // bind data to entity
         foreach ( $cleanedParams as $field => $value ){
